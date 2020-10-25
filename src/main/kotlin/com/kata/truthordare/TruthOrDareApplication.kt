9fc675bd2691a29616bd1a11c1b13c66c331dfea
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2020-10-25 21:36:42 +0000

    First commit

---
+++ src/main/kotlin/com/kata/truthordare/TruthOrDareApplication.kt
@@ -0,0 +1,11 @@
+package com.kata.truthordare
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TruthOrDareApplication
+
+fun main(args: Array<String>) {
+	runApplication<TruthOrDareApplication>(*args)
+}
---
+++ src/test/kotlin/com/kata/truthordare/TruthOrDareApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kata.truthordare
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TruthOrDareApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
