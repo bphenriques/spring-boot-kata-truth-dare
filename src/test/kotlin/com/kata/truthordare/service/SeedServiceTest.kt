3b0215957d309540c2e75880b0dfa78bd0ebe41e
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2020-11-30 20:53:17 +0000

    Exercise 4 - Unit Test complete

--- src/test/kotlin/com/kata/truthordare/service/SeedServiceTest.kt
+++ src/test/kotlin/com/kata/truthordare/service/SeedServiceTest.kt
@@ -1,16 +1,47 @@
 package com.kata.truthordare.service
 
+import com.kata.truthordare.gateway.RandommerGateway
+import com.kata.truthordare.model.ChallengeType
+import com.kata.truthordare.model.TruthDareChallenge
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import com.nhaarman.mockitokotlin2.whenever
 import org.junit.jupiter.api.Test
 
 class SeedServiceTest {
 
+    private val challengesService = mock<ChallengesService>()
+    private val randommerGateway = mock<RandommerGateway>()
+
+    private val subject = SeedService(
+        challengesService = challengesService,
+        randommerGateway = randommerGateway
+    )
+
     @Test
     fun `populate - It does nothing when the number is 0`() {
-        //TODO
+        subject.populate(ChallengeType.TRUTH, 0)
+
+        verifyNoMoreInteractions(challengesService, randommerGateway)
     }
 
     @Test
     fun `populate - It fetches _number_ challenges from the randommer API and registers them`() {
-        //TODO
+        val challenge1 = TruthDareChallenge("hello-1", ChallengeType.TRUTH)
+        val challenge2 = TruthDareChallenge("hello-2", ChallengeType.TRUTH)
+        whenever(randommerGateway.getTruthDareChallenge(eq(challenge1.type))).thenReturn(
+            challenge1,
+            challenge2
+        )
+
+        subject.populate(ChallengeType.TRUTH, 2)
+
+        verify(randommerGateway, times(2)).getTruthDareChallenge(eq(ChallengeType.TRUTH))
+        verify(challengesService, times(1)).add(eq(challenge1))
+        verify(challengesService, times(1)).add(eq(challenge2))
+        verifyNoMoreInteractions(challengesService, randommerGateway)
     }
 }
